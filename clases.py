86532159dd3dd0f12eed9d7aeafb4b63865ae156
Toni2Morales <107318721+Toni2Morales@users.noreply.github.com>
2022-07-07 02:15:41 +0200

    Versión inicial

---
+++ clases.py
@@ -0,0 +1,78 @@
+import numpy as np
+import time
+from IPython.display import clear_output
+class tablero:
+    def __init__(self):
+        self.valor = np.full([10,10], fill_value = "  ")
+    def mostrar(self):
+        print(self.valor)
+    def limpiar(self):
+        self.valor = np.full([10,10], fill_value = "  ")
+class barco:
+    def comprobacion(self):
+        if any([(0 > self.posicion[-1][0]),(self.posicion[-1][0] >= 10),(0 > self.posicion[-1][1]),(self.posicion[-1][1] >= 10), (self.posicion[-1] in (i for x in self.dueño_barco.barcos for i in x.posicion))]):
+            self.posicion.append(["No", "No"])
+            return 0
+    def __init__(self, eslora, posicion, orientacion, jugador):
+        self.eslora = eslora
+        self.posicion = posicion
+        self.orientacion = orientacion
+        self.dueño_barco = jugador
+        while len(self.posicion) != self.eslora:
+            if self.orientacion == "N":
+                for i in range(self.eslora-1):
+                    if self.comprobacion() == 0:
+                        return
+                    self.posicion.append([self.posicion[-1][0] - 1, self.posicion[-1][1]])
+            elif self.orientacion == "S":       
+                for i in range(self.eslora-1):
+                    if self.comprobacion() == 0:
+                        return
+                    self.posicion.append([self.posicion[-1][0] + 1, self.posicion[-1][1]])
+            elif self.orientacion == "O":    
+                for i in range(self.eslora-1):
+                    if self.comprobacion() == 0:
+                        return
+                    self.posicion.append([self.posicion[-1][0], self.posicion[-1][1] - 1])
+            elif self.orientacion == "E":
+                for i in range(self.eslora-1):
+                    if self.comprobacion() == 0:
+                        return
+                    self.posicion.append([self.posicion[-1][0], self.posicion[-1][1] + 1])
+        self.comprobacion()                       
+class jugador:
+    def __init__(self, nombre,):
+        self.error_barco = False
+        self.nombre = nombre
+        self.n_barcos = 20
+        self.tablero_prop = tablero()
+        self.tablero_adver = tablero()
+        self.barcos =[]
+    def anadir_barcos(self):
+        if self.nombre == "Jugador1":
+            for i in range(10):
+                self.lista_eslora = [4,3,3,2,2,2,1,1,1,1]
+                self.barcos.append(barco(self.lista_eslora[i], [[int(input("Introduce la coordenada Y del barco")),int(input("Introduce la coordenada X del barco"))]], input("Introduce la orientación del barco").upper(), self))
+                if ["No", "No"] in (i for x in self.barcos for i in x.posicion):
+                    self.barcos.clear()
+                    self.tablero_prop.limpiar()
+                    return
+                self.colocar_barcos()
+                print("Barco", i + 1)
+                self.tablero_prop.mostrar()
+                clear_output(wait = True)
+        elif self.nombre == "Jugador2":
+            for i in range (10):
+                self.lista_eslora = [4,3,3,2,2,2,1,1,1,1]
+                self.dict_orientaciones = {1:"N", 2: "S", 3:"E", 4:"O"}
+                self.barcos.append(barco(self.lista_eslora[i],[[np.random.randint(0,10), np.random.randint(0,10)]], self.dict_orientaciones[np.random.randint(1,5)], self))
+                if ["No", "No"] in (i for x in self.barcos for i in x.posicion):
+                    self.barcos.clear()
+                    self.tablero_prop.limpiar()
+                    return
+                self.colocar_barcos()
+                clear_output(wait = True)
+    def colocar_barcos(self):
+        for bar in self.barcos:
+            for posicion  in bar.posicion:
+                self.tablero_prop.valor[posicion[0]][posicion[1]] = "O "
---
+++ main.py
@@ -0,0 +1,105 @@
+from clases import *
+import numpy as np
+import time
+from IPython.display import clear_output
+def atacar(self, y, x):
+    jug_atac = None
+    acierto = 0
+    if self.nombre == Jugador1.nombre:
+        jug_atac = Maquina
+    elif self.nombre == Maquina.nombre:
+        jug_atac = Jugador1
+    if jug_atac.tablero_prop.valor[y,x] == "  ":
+        self.tablero_adver.valor[y,x] = "\U0001F4A6"
+        if self.nombre == Jugador1.nombre:
+            print("¡Has atacado en [", y, "," ,x, "]!")
+            print("¡Agua!")
+        elif self.nombre == Maquina.nombre:
+            print("¡Te atacaron en [", y, "," ,x, "]!")
+            print("¡Agua!")
+    elif jug_atac.tablero_prop.valor[y,x] == "O ":
+        jug_atac.tablero_prop.valor[y,x] = "\U0001F4A2"
+        self.tablero_adver.valor[y,x] = "\U0001F4A2"
+        acierto = 1
+        tocado_y_hundido = 0
+        for bar in jug_atac.barcos:
+            comparador = 0
+            for posicion in bar.posicion:
+                if jug_atac.tablero_prop.valor[tuple(posicion)] == "\U0001F4A2":
+                        comparador += 1        
+                if comparador == bar.eslora:
+                    print("¡Tocado y Hundido!")
+                    tocado_y_hundido = 1
+                    for posicion in bar.posicion:
+                        jug_atac.tablero_prop.valor[tuple(posicion)] = "\U0001F4A5"
+                        self.tablero_adver.valor[tuple(posicion)] = "\U0001F4A5"
+                        jug_atac.n_barcos -= 1
+        if tocado_y_hundido == 0:
+            if self.nombre == Jugador1.nombre:
+                        print("¡Has atacado en [", y, "," ,x, "]!")
+                        print("!Tocado¡")
+            elif self.nombre == Maquina.nombre:
+                        print("¡Te atacaron en [", y, "," ,x, "]!")
+                        print("!Tocado¡")
+    print("Tablero de Jugador2")
+    Maquina.tablero_prop.mostrar()
+    print("Tu Tablero:")
+    Jugador1.tablero_prop.mostrar()
+    print("Tus ataques:")
+    Jugador1.tablero_adver.mostrar()
+    clear_output(wait=True)
+    return acierto
+def acierto(self):
+    if (Jugador1.n_barcos != 0) and (Maquina.n_barcos != 0):
+        if self.nombre == Jugador1.nombre:
+            if atacar(Jugador1,int(input("Introduce la coordenada Y para atacar")),int(input("Introduce la coordenada X para atacar"))) == 1:
+                acierto((self))
+        if self.nombre == Maquina.nombre:
+            if atacar(Maquina, np.random.randint(10), np.random.randint(10)) == 1:
+                acierto((self))
+Jugador1 = jugador("Jugador1")
+Jugador1.anadir_barcos()
+while len(Jugador1.barcos) != 10:
+    print("Introduce unas coordenadas correctas")
+    Jugador1.anadir_barcos()
+Maquina = jugador("Jugador2")
+while len(Maquina.barcos) != 10:
+    Maquina.anadir_barcos()
+time.sleep(0.1)
+while (Jugador1.n_barcos != 0) and (Maquina.n_barcos != 0):
+    turnos = (1,2)
+    for i in turnos:
+        if i == 1:
+            if (Jugador1.n_barcos != 0) and (Maquina.n_barcos != 0):
+                time.sleep(4)
+                print("turno de Jugador1")
+                print("Tablero de Jugador2")
+                Maquina.tablero_prop.mostrar()
+                print("Tu Tablero:")
+                Jugador1.tablero_prop.mostrar()
+                print("Tus ataques:")
+                Jugador1.tablero_adver.mostrar()
+                clear_output(wait=True)
+                acierto(Jugador1)
+        elif i == 2:
+            if (Jugador1.n_barcos != 0) and (Maquina.n_barcos != 0):
+                time.sleep(5)
+                print("turno de Jugador 2")
+                time.sleep(2)
+                acierto(Maquina)
+time.sleep(0.01)
+if Jugador1.n_barcos == 0:
+    print("\U0001F38A" + "¡" + Maquina.nombre + " ha ganado!" + "\U0001F38A")
+    print("\U0001F44E" + "¡" + Jugador1.nombre + " ha perdido!" + "\U0001F44E")
+elif Maquina.n_barcos == 0:
+    print("\U0001F38A" + "¡" + Jugador1.nombre + " ha ganado!" + "\U0001F38A")
+    print("\U0001F44E" + "¡" + Maquina.nombre + " ha perdido!" + "\U0001F44E")
+print("Tableros:")
+print("Tablero de " + Jugador1.nombre)
+Jugador1.tablero_prop.mostrar()
+print("Ataques de " + Jugador1.nombre)
+Jugador1.tablero_adver.mostrar()
+print("Tablero de " + Maquina.nombre)
+Maquina.tablero_prop.mostrar()
+print("Ataques de " + Maquina.nombre)
+Maquina.tablero_adver.mostrar()
